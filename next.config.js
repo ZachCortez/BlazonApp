aefa38a39ddb2b640cd5b3454e28cb4164d02f40
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2022-05-08 11:49:07 -0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  images: {
+  domains: [
+  "links.papareact.com",
+  "platform-lookaside.fbsbx.com",
+  "firebasestorage.googleapis.com",
+  "scontent-sea1-1.xx.fbcdn.net",
+  ],
+}
+};
